4df00a20a345a5e1fda2cc83a2b17a75d11cecbc
valery-kiselev <83948789+valery-kiselev@users.noreply.github.com>
2024-12-17 17:11:15 +0400

    Update user.py

--- 17_2/app/models/user.py
+++ 17_2/app/models/user.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from app.backend.db import Base
-from app.models.task import *
+
 
 
 class User(Base):
     __tablename__ = "users" # Имя таблицы в базе данных
+    __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, index=True) # Уникальный идентификатор
     username = Column(String)# Имя пользователя
     firstname = Column(String)
@@ -16,3 +17,4 @@ class User(Base):
 
 from sqlalchemy.schema import CreateTable
 print(CreateTable(User.__table__))
+
